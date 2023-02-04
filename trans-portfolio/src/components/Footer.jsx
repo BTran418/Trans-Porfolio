efac54a80d5f0ea4077ca783e9dcf86cc037ec1f
Brendan Tran <btran418@gmail.com>
2023-02-04 03:36:15 -0800

    Finished base portfolio

--- trans-portfolio/src/components/Footer.jsx
+++ trans-portfolio/src/components/Footer.jsx
@@ -2,7 +2,14 @@ import React from "react";
 
 function Footer() {
     return (
-     <div>Footer</div>
+     <div>
+        {/*Social Media Links*/}
+        <p className="text-sm mt-2 opacity-50 text-center">
+            &copy; {new Date().getFullYear()} {" "}
+            Brendan Tran
+            All rights reserved
+        </p>
+     </div>
     )
 }
 
