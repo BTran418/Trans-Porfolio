351005af867f1cdbe6f15b861f060911b0e0c3df
Brendan Tran <btran418@gmail.com>
2023-02-04 15:14:42 -0800

    Configured darkmode settings

--- trans-portfolio/tailwind.config.cjs
+++ trans-portfolio/tailwind.config.cjs
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html","./src/**/*.{jsx,js}"],
-  darkmode: "class",
+  darkMode: "class",
   theme: {
     extend: {
       fontFamily:{
