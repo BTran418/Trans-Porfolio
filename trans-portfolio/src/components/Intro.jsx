0c21bbd540abd7671f82eec391f7b07044109d45
Brendan Tran <btran418@gmail.com>
2023-02-04 01:54:37 -0800

    Updated the intro.jsx

--- trans-portfolio/src/components/Intro.jsx
+++ trans-portfolio/src/components/Intro.jsx
@@ -2,7 +2,11 @@ import React from "react";
 
 function Intro() {
     return (
-     <div>Intro</div>
+     <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
+        <h1 className="text-4xl md:text-7xl mb-1 md:mb-3 font-bold">Brendan Tran</h1>
+        <p className="text-base md:text-xl mb-3 font-medium">Full Stack Software Developer</p>
+        <p className="text-small max-w-xl mb-6 font-bold">I am an enthusiastic, and driven team-player with strong technical skills. I am most passionate in  creating and executing innovative software solutions to enhance business productivity and further grow and improve my problem-solving skills in a professional environment. </p>
+     </div>
     )
 }
 
