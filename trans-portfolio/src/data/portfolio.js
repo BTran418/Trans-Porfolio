94ca766d2c91eae7e06bc5cb22655b08768a17e7
Brendan Tran <btran418@gmail.com>
2023-02-04 02:36:01 -0800

    Portfolio updated

---
+++ trans-portfolio/src/data/portfolio.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        title: 'Developer Portfolio',
+        imgUrl: '',
+        stack: ['React', 'Vite', 'Node.js', 'Tailwind', 'Javascript'],
+        link: '',
+    },
+    {
+        title: 'Infinite Pets',
+        imgUrl: '',
+        stack: ['HTML', 'CSS', 'JSX', 'Java', 'MySql'],
+        link: '',
+    },
+    {
+        title: 'Angular Project',
+        imgUrl: '',
+        stack: ['Agnular', 'Node.js', 'Tailwind', 'Javascript'],
+        link: '',
+    },
+    {
+        title: 'BattleShip Game',
+        imgUrl: '',
+        stack: ['Python'],
+        link: '',
+    }
+]
