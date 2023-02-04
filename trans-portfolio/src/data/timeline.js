02a3e60ae7abeb338aa631dde784f114a4394905
Brendan Tran <btran418@gmail.com>
2023-02-04 03:13:35 -0800

    Updated Timeline compenents

---
+++ trans-portfolio/src/data/timeline.js
@@ -0,0 +1,27 @@
+export default [
+    {
+        year: '2023',
+        title: "Today's Weather",
+        duration: '1 Week',
+        details: "Designed a basic work portfolio that holds all of the projects I've worked on."
+    },
+    {
+        year: '2021',
+        title: "Infinite Pets",
+        duration: '4 Months',
+        details: "Consults using waterfall and agile methodologies to design and develop a new multithreading scheduling system using MVC architecture, Designed and developed front end webpages using JSP's written using HTML5. JPA, jQuery  and styled with CSS, Designed and developed backend database written in Java and MySQL, Organized, updated and transferred documents within team members using GitHub, Managed project deadlines and team tasks assignments through Microsoft Project and Trello"
+        
+    },
+    {
+        year: '2020',
+        title: "Transcend Yoga",
+        duration: '4 Months',
+        details: "Consults using waterfall and agile methodologies to design a new multithreading scheduling system using MVC architecture, Designed front end webpages using Adobe XD and wireframes for realistic UX and UI, Designed backend database using Java and MySQL, Organized, updated and transferred documents within team members using GitHub, Managed project deadlines and team tasks assignments through Microsoft Project" 
+    },
+    {
+        year: '2019',
+        title: 'Weather Dashboard',
+        duration: '3 Years',
+        details: "Client-Server Admin, Scripting for System Admin, Server Service Admin, Data Center Management and Virtualization, Web Development Essentials, IT Security Essentials, Project Management"
+    }
+]
