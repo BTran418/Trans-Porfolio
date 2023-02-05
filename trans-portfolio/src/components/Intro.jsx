6951294efc7eee9e430601158c0fc1bd19fd01bf
Brendan Tran <btran418@gmail.com>
2023-02-04 16:00:59 -0800

    Completed implementing dark mode in all components

--- trans-portfolio/src/components/Intro.jsx
+++ trans-portfolio/src/components/Intro.jsx
@@ -3,9 +3,9 @@ import React from "react";
 function Intro() {
     return (
      <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
-        <h1 className="text-4xl md:text-7xl mb-1 md:mb-3 font-bold">Brendan Tran</h1>
-        <p className="text-base md:text-xl mb-3 font-medium">Full Stack Software Developer</p>
-        <p className="text-small max-w-xl mb-6 font-bold">I am an enthusiastic, and driven team-player with strong technical skills. I am most passionate in  creating and executing innovative software solutions to enhance business productivity and further grow and improve my problem-solving skills in a professional environment. </p>
+        <h1 className="text-4xl md:text-7xl mb-1 md:mb-3 font-bold dark:text-white">Brendan Tran</h1>
+        <p className="text-base md:text-xl mb-3 font-medium dark:text-stone-300">Full Stack Software Developer</p>
+        <p className="text-small max-w-xl mb-6 font-bold dark:text-stone-100">I am an enthusiastic, and driven team-player with strong technical skills. I am most passionate in  creating and executing innovative software solutions to enhance business productivity and further grow and improve my problem-solving skills in a professional environment. </p>
      </div>
     )
 }
--- trans-portfolio/src/components/TimelineItem.jsx
+++ trans-portfolio/src/components/TimelineItem.jsx
@@ -5,12 +5,12 @@ function TimelineItem({year, title, duration, details}) {
         <ol className="flex flex-col md-flex-row relative border-l border-stone-200">
             <li mb-10 ml-4>
                 <div className="absolute w-3 h-3 bg-stone-200 rounded-full mt-1.5 -left-1.5 border border-white"></div>
-                <p className="flex flex-wrap gap-4 flex-row items-center justify-start text-xs md:text-sm">
-                    <span className="inline-block px-2 py-1 font-semibold text-white bg-stone-900 rounded-md">{year}</span>
-                    <h3 className="text-lg font-semibold text-stone-900">{title}</h3>\
-                    <div className="my-1 text-sm font-normal leading-none text-stone-400">{duration}</div>
+                <p className="flex flex-wrap gap-4 flex-row items-center justify-start text-xs md:text-sm px-2.5">
+                    <span className="inline-block px-2 py-1 font-semibold text-white bg-stone-900 rounded-md dark:bg-stone-500">{year}</span>
+                    <h3 className="text-lg font-semibold text-stone-900 dark:text-white">{title}</h3>\
+                    <div className="my-1 text-sm font-normal leading-none text-stone-400 dark:text-stone-300">{duration}</div>
                 </p>
-                <p className="my-2 text-base font-normal text-stone-500">{details}</p>
+                <p className="my-2 text-base font-normal text-stone-500 dark:text-stone-100 px-2">{details}</p>
             </li>
         </ol>
     )
